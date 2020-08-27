d5ed152bb5b0a1514611bcd8acbbb664f60e1772
miiwu <i.miiwu@outlook.com>
2020-08-28 00:24:09 +0800

    feat: support table `filter(val, prnt)`

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,46 @@
 import { construct_table } from "../src/sspanel.soulsign.js";
 
-const assert_table = [
+const table = [
+    /* 属性 */
+    "layer_0",
+    "layer_1",
+    "layer_2",
+    /* 子属性 */
+    ["layer_0_0", "layer_0_1", ["layer_0_0_0", "layer_0_0_1", [], []], []],
+    ["layer_1_0", "layer_1_1"],
+    [],
+    /* 过滤器 */
+    [
+        [
+            [
+                function () {
+                    return { source: "0_0_0" };
+                },
+            ],
+        ],
+        [
+            function () {
+                return { source: "0_1" };
+            },
+        ],
+    ],
+    [
+        function () {
+            return { source: "1_0" };
+        },
+        function () {
+            return { source: "1_1" };
+        },
+    ],
+    [],
+];
+let target = {};
+
+construct_table(target, table);
+console.log("target:", target);
+console.log("\r");
+
+const variable_table = [
         "domain",
         "keyword",
         "path",
@@ -10,17 +50,38 @@ const assert_table = [
         ["log", "sign"],
         ["get_log_in", "post_sign_in", "notify_sign_in"],
     ],
-    variable_tables = [...assert_table],
-    config_table = ["site", "param", [...assert_table], [...assert_table]];
+    config_table = [
+        ...variable_table,
+        [
+            function (value, parent) {
+                console.log("value:", value, "parent:", parent);
+                return { code: 0 };
+            },
+        ],
+    ],
+    assert_table = [
+        ...variable_table,
+        [
+            function (value) {
+                if (value instanceof Array && value.length) return { source: true };
+                else return { source: false };
+            },
+        ],
+    ];
 
 let configs = {},
     asserts = {},
     variables = {};
 
-let sites;
+console.log(assert_table);
+console.log("\r");
 
-console.log(assert_table, assert_table, config_table);
+console.log(construct_table(configs, config_table, [[[".com", ".net"]], [1, 2], [3, 4], [5, 6, 7]]));
+console.log("configs:", configs);
+console.log("\r");
 
-console.log(construct_table(variables, assert_table));
-console.log(construct_table(asserts, assert_table));
-console.log(construct_table(configs, config_table, [".com", "a", "b", "c", "d", function () {}, , , ".net", "e"]));
+variables = construct_table(asserts, assert_table, JSON.parse(JSON.stringify(configs))); // `深拷贝` configs，仅演示，使用时请自行选择 `深拷贝` 方法
+console.log("asserts:", asserts);
+console.log("\r");
+
+console.log("variables:", variables);
--- src/sspanel.soulsign.js
+++ src/sspanel.soulsign.js
@@ -26,17 +26,83 @@ function operate_item(index, table, operator = function (item, index) {}) {
 
 function construct_table(target, table, source = [], nest = false) {
     function endpoint() {
-        let source_e = source[source.index++];
-        return !!source_e ? source_e : {};
+        let fil_e = _A_.filter.func(_A_.property());
+        _A_.index.src++;
+        return false === fil_e || 0 === fil_e || !!fil_e ? fil_e : {};
     }
 
-    let key_ct = 0;
+    /* 非嵌套，添加辅助属性 */
+    if (!nest) {
+        source._assist_ = {
+            index: { src: 0, ep: -1 },
+            key: [],
+            map: table,
+            nest: [],
+            property: [
+                function () {
+                    let map_p = _A_.map,
+                        nest_p = _A_.nest,
+                        path_p = nest_p.slice(-1),
+                        prnt_p = "";
 
-    if (!nest) source.index = 0; // 非嵌套，添加索引属性
+                    for (let idx = 0, len = nest_p.length - 1; idx < len; idx++) {
+                        prnt_p += `.${map_p[nest_p[idx].key]}`;
+                        map_p = map_p[nest_p[idx].val];
+                    } // 计算出父对象路径
+
+                    path_p = prnt_p + `.${map_p[path_p[0].key]}`; // 计算出路径
+
+                    return { val: eval(`source${path_p}`), prnt: eval(`source${prnt_p}`) };
+                },
+                function () {
+                    let index_p = _A_.index;
+                    return {
+                        val: source.length && !!source[index_p.ep] ? source[index_p.ep][index_p.src] : {},
+                    };
+                },
+            ][Number(source instanceof Array)],
+            filter: {
+                cfg: {
+                    tab: [],
+                    idx: 0,
+                    push(index, key) {
+                        if (!this.idx) this.idx = key << 1;
+                        this.tab.push(
+                            this.tab.length ? this.tab.slice(-1)[0][index - key] : _A_.map[index - key + this.idx]
+                        );
+                    },
+                    pop() {
+                        this.tab.pop();
+                    },
+                },
+                func(prop) {
+                    let fil_f = {};
+                    try {
+                        fil_f = this.cfg.tab.slice(-1)[0][_A_.index.src];
+                        fil_f = fil_f ? fil_f(prop.val, prop.prnt) : { code: 0 };
+                    } catch (exception) {
+                        fil_f = {};
+                    } finally {
+                        return fil_f.hasOwnProperty("source") ? fil_f.source : fil_f.code ? {} : prop.val;
+                    }
+                },
+            },
+        };
+    }
+
+    let _A_ = source._assist_,
+        key_ct = 0;
 
     operate_table(table, function (item, idx) {
         if ("object" == typeof item) {
-            target[table[idx - key_ct]] = construct_table(target[table[idx - key_ct]], item, source, true);
+            _A_.key.push(key_ct);
+            if (!_A_.key.length || [..._A_.key].pop() << 1 > idx) {
+                _A_.nest.push({ key: idx - key_ct, val: idx, flt: idx << 1, nest: 0 });
+                _A_.filter.cfg.push(idx, key_ct);
+                target[table[idx - key_ct]] = construct_table(target[table[idx - key_ct]], item, source, _A_);
+                _A_.filter.cfg.pop();
+                _A_.nest.pop();
+            }
         } else {
             key_ct++;
             target[table[idx]] = {}; // 如果是 字符串 ，创建对象
@@ -44,11 +110,24 @@ function construct_table(target, table, source = [], nest = false) {
     });
 
     if (key_ct == table.length) {
-        if (!key_ct) target = endpoint();
-        else for (let idx = 0; idx < key_ct; idx++) target[table[idx]] = endpoint();
+        _A_.index.src = 0;
+        _A_.index.ep++;
+
+        if (key_ct) {
+            let nest_step = _A_.nest.push({ key: 0, val: 0 }) - 1;
+            for (let idx = 0; idx < key_ct; idx++) {
+                _A_.nest[nest_step].key = idx;
+                target[table[idx]] = endpoint();
+            }
+            _A_.nest.pop();
+        } else target = endpoint();
     } // 端点
 
-    return target;
+    /* 非嵌套，删除辅助属性 */
+    if (!nest) {
+        delete source._assist_;
+        return source;
+    } else return target;
 }
 
 export { operate_table, operate_item, construct_table };
